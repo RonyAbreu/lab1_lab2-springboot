380d03357785af64009b96cb0f93167b3360b017
RonyAbreu <ronyelison78@gmail.com>
2023-07-26 09:45:20 -0300

    add news

--- src/main/java/br/ufpb/dcx/lab1/controller/DisciplinaController.java
+++ src/main/java/br/ufpb/dcx/lab1/controller/DisciplinaController.java
@@ -1,8 +1,14 @@
 package br.ufpb.dcx.lab1.controller;
 
+import br.ufpb.dcx.lab1.services.DisciplinaServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/disciplina")
 public class DisciplinaController {
+    @Autowired
+    private DisciplinaServices disciplinaServices;
 
 }
---
+++ src/main/java/br/ufpb/dcx/lab1/entities/DisciplinaException.java
@@ -0,0 +1,7 @@
+package br.ufpb.dcx.lab1.entities;
+
+public class DisciplinaException extends RuntimeException{
+    public DisciplinaException(String msg){
+        super(msg);
+    }
+}
