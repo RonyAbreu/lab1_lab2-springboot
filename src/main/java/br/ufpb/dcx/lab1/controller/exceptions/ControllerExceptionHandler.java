14fcfd78557a4b0def94c6d7c32658b2bc00384a
RonyAbreu <ronyelison78@gmail.com>
2023-08-15 21:40:02 -0300

    criação da classe para controlar todos os erros gerados pelas requisições

---
+++ src/main/java/br/ufpb/dcx/lab1/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.ufpb.dcx.lab1.controller.exceptions;
+
+import br.ufpb.dcx.lab1.services.exceptions.DisciplinaNotFound;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DisciplinaNotFound.class)
+    public ResponseEntity<StandardError> disciplinaNotFound(DisciplinaNotFound e, HttpServletRequest request){
+        String error = "Not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
