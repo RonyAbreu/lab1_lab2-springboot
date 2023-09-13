006d600c593eecbcc3301d5ead041faeb3f607ea
RonyAbreu <ronyelison78@gmail.com>
2023-09-13 09:40:13 -0300

    atualizações no ControllerExceptionHandler

--- src/main/java/br/ufpb/dcx/lab/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/ufpb/dcx/lab/controller/exceptions/ControllerExceptionHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,18 +22,23 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler{
     public ResponseEntity<StandardError> disciplinaNotFound(DisciplinaNotFound e, HttpServletRequest request){
         String error = "Not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        StandardError err = new StandardError(error,status.value(),status.getReasonPhrase());
         return ResponseEntity.status(status).body(err);
     }
 
 
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
-        StandardError err = new StandardError(Instant.now(),status.value(),"error",ex.getMessage(),request.getContextPath());
-        return new ResponseEntity<>(err,status);
+        List<ErrorObject> errors = getErros(ex);
+        StandardError errorResponse = getErrorResponse(ex,status,errors);
+        return ResponseEntity.status(status).body(errorResponse);
     }
 
     private List<ErrorObject> getErros(MethodArgumentNotValidException ex){
         return ex.getBindingResult().getFieldErrors()
                 .stream().map(error -> new ErrorObject(error.getDefaultMessage(),error.getField())).toList();
+    }
+
+    private StandardError getErrorResponse(MethodArgumentNotValidException ex, HttpStatus status, List<ErrorObject> erros){
+        return new StandardError("Requisição possui campos inválidos", status.value(),status.getReasonPhrase(),ex.getBindingResult().getObjectName(),erros);
     }
 }
