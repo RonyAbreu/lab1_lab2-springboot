1b430a2b72cd65eb100f7eb7e88ccd02bb91279d
RonyAbreu <ronyelison78@gmail.com>
2023-09-14 12:43:17 -0300

    povoando o banco H2

--- src/main/java/br/ufpb/dcx/lab/config/TestConfig.java
+++ src/main/java/br/ufpb/dcx/lab/config/TestConfig.java
@@ -34,34 +34,30 @@ public class TestConfig implements CommandLineRunner {
         Disciplina dis2 = new Disciplina(null,"Matemática",0);
         Disciplina dis3 = new Disciplina(null,"História",0);
 
-        disciplinaDAORepository.saveAll(Arrays.asList(dis1,dis2,dis3));
-
         Comentario c1 = new Comentario(null, LocalDate.parse("11/10/2000",dtf),"Eu não gosto de português",false,dis1);
         Comentario c2 = new Comentario(null, LocalDate.now(),"Muito bom",false,dis2);
         Comentario c3 = new Comentario(null, LocalDate.parse("01/01/2001",dtf),"Gostei bastante",false,dis3);
 
-        comentarioDAORepository.saveAll(Arrays.asList(c1,c2,c3));
-
         dis1.getComentarios().add(c1);
         dis2.getComentarios().add(c2);
         dis3.getComentarios().add(c3);
+
         disciplinaDAORepository.saveAll(Arrays.asList(dis1,dis2,dis3));
+        comentarioDAORepository.saveAll(Arrays.asList(c1,c2,c3));
 
         Tag t1 = new Tag(null, "Ruim");
         Tag t2 = new Tag(null, "Massante");
         Tag t3 = new Tag(null, "Excelente");
 
-        tagDAORepository.saveAll(Arrays.asList(t1,t2,t3));
-
         dis1.getTags().add(t1);
         dis2.getTags().add(t2);
         dis3.getTags().add(t3);
 
-        disciplinaDAORepository.saveAll(Arrays.asList(dis1,dis2,dis3));
+        tagDAORepository.saveAll(Arrays.asList(t1,t2,t3));
 
         List<Double> notas1 = new ArrayList<>(Arrays.asList(7.7,10.0,8.4));
         dis1.getNotas().addAll(notas1);
 
-        disciplinaDAORepository.save(dis1);
+        disciplinaDAORepository.saveAll(Arrays.asList(dis1,dis2,dis3));
     }
 }
