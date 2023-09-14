a07ca48c4182e7fefaecb7ecfacba1b415ce3ac2
RonyAbreu <ronyelison78@gmail.com>
2023-09-14 20:05:20 -0300

    criação da classe TagDTO

---
+++ src/main/java/br/ufpb/dcx/lab/dto/TagDTO.java
@@ -0,0 +1,20 @@
+package br.ufpb.dcx.lab.dto;
+
+import br.ufpb.dcx.lab.entities.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@NoArgsConstructor
+@Data
+public class TagDTO implements Serializable {
+    @NotBlank(message = "O campo (nome) não pode estar vazio.")
+    @Size(min = 2, max = 15, message = "O campo (nome) excedeu o limite de caracteres.")
+    private String tagName;
+
+    public TagDTO(Tag tag) {
+        this.tagName = tag.getName();
+    }
+}
