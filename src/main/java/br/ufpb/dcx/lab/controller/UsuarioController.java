fb1e75095ab76bb8c3bba697fba162f97b6789fe
RonyAbreu <ronyelison78@gmail.com>
2023-10-11 11:38:37 -0300

    criação do Controlador de Usuarios

---
+++ src/main/java/br/ufpb/dcx/lab/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package br.ufpb.dcx.lab.controller;
+
+import br.ufpb.dcx.lab.entities.Usuario;
+import br.ufpb.dcx.lab.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/auth/usuarios")
+public class UsuarioController {
+    private UsuarioService service;
+    @Autowired
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{email}")
+    public ResponseEntity<Usuario> buscaUsuario(@PathVariable String email, @RequestHeader("Authorization") String header){
+        var usuarioRetornado = service.buscaUsuario(header,email);
+        return ResponseEntity.ok(usuarioRetornado);
+    }
+
+    @DeleteMapping(value = "/{email}")
+    public ResponseEntity<Void> removeUsuario(@PathVariable String email, @RequestHeader("Authorization") String header){
+        service.removeUsuario(header, email);
+        return ResponseEntity.ok().build();
+    }
+}
